bb2d02ffce43e0865a21f9235939f7f0c7041e25
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-18 16:20:02 -0300

    Criação da classe departamento

---
+++ Aula 04/src/main/java/org/serratec/Exercicio03/Domain/Departamento.java
@@ -0,0 +1,63 @@
+package org.serratec.Exercicio03.Domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 20)
+    private String nome;
+
+    @Column(name = "setor", nullable = false, length = 20)
+    private String setor;
+
+    @Column(name = "gerente", nullable = false,length = 40)
+    private String gerente;
+
+    public Departamento(Long cod, String gerente, String nome, String setor) {
+        this.cod = cod;
+        this.gerente = gerente;
+        this.nome = nome;
+        this.setor = setor;
+    }
+
+    public Departamento() {
+
+    }
+
+    public Long getCod() {
+        return cod;
+    }
+
+    public void setCod(Long cod) {
+        this.cod = cod;
+    }
+
+    public String getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(String gerente) {
+        this.gerente = gerente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+}
