f591d8ea8b56741f0c794608744b495a026e6b6c
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-17 22:55:02 -0300

    Criando repositório de funcionário

--- Aula 03/Exercicio03/src/main/java/org/serratec/Exercicio03/Repository/FuncionarioRepository.java
+++ Aula 03/Exercicio03/src/main/java/org/serratec/Exercicio03/Repository/FuncionarioRepository.java
@@ -1,9 +1,11 @@
 package org.serratec.Exercicio03.Repository;
 
 
+import org.serratec.Exercicio03.Domain.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository {
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 
 }
