b95b18abc9e77bee250326bddbd2f782d49962a6
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-17 22:55:43 -0300

    Criando controller funcionario

--- Aula 03/Exercicio03/src/main/java/org/serratec/Exercicio03/Controller/FuncionarioController.java
+++ Aula 03/Exercicio03/src/main/java/org/serratec/Exercicio03/Controller/FuncionarioController.java
@@ -2,27 +2,66 @@ package org.serratec.Exercicio03.Controller;
 
 
 import org.serratec.Exercicio03.Domain.Funcionario;
+import org.serratec.Exercicio03.Repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/funcionario")
 public class FuncionarioController {
 
-    static List<Funcionario> listaFuncionario = new ArrayList<>();
+   @Autowired
+    private FuncionarioRepository funcionarioRepository;
 
-    static {
-        listaFuncionario.add(new Funcionario(12341L, "João", "Vendedor"));
-        listaFuncionario.add(new Funcionario(12342L, "Pedro", "Vendedor"));
-        listaFuncionario.add(new Funcionario(12343L, "Marcelo", "Vendedor"));
-        listaFuncionario.add(new Funcionario(12344L, "Julia", "Estoquista"));
-        listaFuncionario.add(new Funcionario(12345L, "Paulo", "Estoquista"));
-        listaFuncionario.add(new Funcionario(12346L, "Marcos", "Gerente"));
-        listaFuncionario.add(new Funcionario(12347L, "Eduarda", "Proprietário"));
-    }
+   @GetMapping
+    public List<Funcionario> listar(){
+       return funcionarioRepository.findAll();
+   }
 
+   @GetMapping("/{id}")
+   public ResponseEntity<Funcionario> buscarPorId(@PathVariable Long id){
+       Optional<Funcionario> funcionario = funcionarioRepository.findById(id);
+
+       if (funcionario.isPresent()){
+           return ResponseEntity.ok(funcionario.get());
+       }
+       return ResponseEntity.notFound().build();
+   }
+
+   @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Funcionario inserir(@RequestBody Funcionario funcionario){
+       return funcionarioRepository.save(funcionario);
+   }
+
+   @PutMapping("/{id}")
+    public ResponseEntity<Funcionario> editar(@RequestBody Funcionario funcionario, @PathVariable Long id){
+       if (!funcionarioRepository.existsById(id)){
+           return ResponseEntity.notFound().build();
+       }
+       funcionario.setId(id);
+       funcionario = funcionarioRepository.save(funcionario);
+       return ResponseEntity.ok(funcionario);
+   }
+
+   @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+       if (!funcionarioRepository.existsById(id)){
+           return ResponseEntity.notFound().build();
+       }
+
+       funcionarioRepository.deleteById(id);
+       return ResponseEntity.noContent().build();
+   }
 }
+
+
+
+
