1395120f418fd6d11386f2d2fd21436efa890c16
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-18 22:58:20 -0300

    Adicionando Exceptions

--- Aula 04/src/main/java/org/serratec/Exercicio03/Exception/ControllerExceptionHandler.java
+++ Aula 04/src/main/java/org/serratec/Exercicio03/Exception/ControllerExceptionHandler.java
@@ -1,4 +1,43 @@
-package org.serratec.Exercicio03.Controller;
+package org.serratec.Exercicio03.Exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroResposta> tratarErroValidacao(MethodArgumentNotValidException ex) {
+        List<String> erros = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(erro -> erro.getField() + ": " + erro.getDefaultMessage())
+                .toList();
+
+        ErroResposta resposta = new ErroResposta(
+                LocalDateTime.now(),
+                "Erro de validação",
+                erros,
+                400
+        );
+
+        return ResponseEntity.status(400).body(resposta);
+    }
+
+    @ExceptionHandler(RecursoNaoEncontradoException.class)
+    public ResponseEntity<ErroResposta> tratarRecursoNaoEncontrado(RecursoNaoEncontradoException ex) {
+        ErroResposta resposta = new ErroResposta(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                List.of(),
+                404
+        );
+        return ResponseEntity.status(404).body(resposta);
+    }
+
 }
