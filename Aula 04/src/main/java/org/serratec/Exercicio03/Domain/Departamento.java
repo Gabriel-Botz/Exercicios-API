a1e250a0a43ef8da3cc51a9b7bdfa46e8af43198
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-18 21:55:46 -0300

    Adicionando campo email, e adicionando anotacoes

--- Aula 04/src/main/java/org/serratec/Exercicio03/Domain/Departamento.java
+++ Aula 04/src/main/java/org/serratec/Exercicio03/Domain/Departamento.java
@@ -1,6 +1,9 @@
 package org.serratec.Exercicio03.Domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Departamento {
@@ -9,32 +12,44 @@ public class Departamento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Column(name = "nome", nullable = false, length = 20)
+    @NotBlank(message = "O nome não pode ser vazio.")
+    @Size(min = 2, max = 50, message = "O nome deve ter entre 2 e 50 caracteres")
     private String nome;
 
     @Column(name = "setor", nullable = false, length = 20)
+    @NotBlank(message = "O setor não pode ser vazio.")
+    @Size(min = 2, max = 50, message = "O setor deve ter entre 2 e 50 caracteres")
     private String setor;
 
     @Column(name = "gerente", nullable = false,length = 40)
+    @NotBlank(message = "O campo 'gerente' não pode ser vazio.")
+    @Size(min = 2, max = 50, message = "O campo 'gerente' deve ter entre 2 e 50 caracteres")
     private String gerente;
 
-    public Departamento(Long id, String gerente, String nome, String setor) {
-        this.id = id;
+    @Column
+    @NotBlank(message = "O campo email não pode estar vazio.")
+    @Email(message = "Email inválido")
+    private String email;
+
+    public Departamento(String email, String gerente, Long id, String nome, String setor) {
+        this.email = email;
         this.gerente = gerente;
+        this.id = id;
         this.nome = nome;
         this.setor = setor;
     }
 
     public Departamento() {
-
     }
 
-    public Long getId() {
-        return id;
+    public String getEmail() {
+        return email;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getGerente() {
@@ -43,6 +58,14 @@ public class Departamento {
 
     public void setGerente(String gerente) {
         this.gerente = gerente;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
